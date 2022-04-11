a26b86fd7cae8c22dbd738896d0f153ca489f457
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-04-11 12:50:35 +0100

    updated data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,18 @@
 USE employee_db;
 
-INSERT INTO department (name) VALUES ("Human Resources");
-INSERT INTO department (name) VALUES ("Marketing");
-INSERT INTO department (name) VALUES ("Information Technology");
-INSERT INTO department (name) VALUES ("Corporate");
+INSERT INTO department (name) VALUES ("Management");
+INSERT INTO department (name) VALUES ("Sales");
+INSERT INTO department (name) VALUES ("Accounts");
+INSERT INTO department (name) VALUES ("Wharehouse");
 
-INSERT INTO role (title, salary, department_id) VALUES ("Analyst", 70, 3);
-INSERT INTO role (title, salary, department_id) VALUES ("Communications Associate", 50, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Social Media Manager", 50, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Director", 100, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Director", 100, 4);
+INSERT INTO role (title, salary, department_id) VALUES ("Warehouse Forman", 70, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Senior Sales Rep", 50, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Accountant", 50, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Assistant Regional Manager", 100, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Regional Manager", 100, 4);
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("David", "Jester", 2);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Abby", "Whiteman", 1);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Katie", "Pieto", 3);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Rachael", "Squirm", 4);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Lizzie", "Halep", 5);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Glynn", "Taffy", 2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Tim", "Canterbury", 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Keith", "Bishop", 3);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Gareth", "Keenan", 4);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("David", "Brent", 5);
